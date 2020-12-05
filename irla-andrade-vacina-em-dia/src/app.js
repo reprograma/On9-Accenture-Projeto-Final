ee684c2cfb6b335662cedeaf983471333c422a15
irlaandrade <paula.irla@gmail.com>
2020-12-05 12:01:38 -0300

    configurações iniciais do projeto

---
+++ irla-andrade-vacina-em-dia/src/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
+
+const index = require('./routes/index')
+const users = require('./routes/user')
+const vaccines = require('./routes/vaccine')
+const admin = require('./routes/admin')
+const login = require('./routes/login')
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', index)
+app.use('/users', users)
+app.use('/vaccines', vaccines)
+app.use('/admin', admin)
+app.use('/login', login)
+
+
+module.exports = app
