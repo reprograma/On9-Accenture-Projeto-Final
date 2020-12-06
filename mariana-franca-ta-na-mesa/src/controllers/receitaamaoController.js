3b8ea8fe82fa0d186bd802412c9626fda5a624c8
Mariana França <donafran@gmail.com>
2020-12-06 15:42:57 -0300

    Atualizações Projeto

--- mariana-franca-ta-na-mesa/src/controllers/receitaamaoController.js
+++ mariana-franca-ta-na-mesa/src/controllers/receitaamaoController.js
@@ -34,7 +34,7 @@ const getByTitle = (request, response) => {
 
 }
 
-const getByingredient = (request, response) => {
+const getByMainIngredient = (request, response) => {
     let { ingredientePrincipal } = request.query
     Recipe.find({ ingredientePrincipal: ingredientePrincipal })
         .then((recipes) => {
@@ -56,6 +56,31 @@ const getByTypeFood = (request,response) => {
             response.status(400).json({ message: "erro" })
         })
 
+}
+
+const getByChoseRecipe = (request, response) => {
+    //if({receitaSelecionada == true}) {
+        //response.status(400).json({ message: "Não existe nenhuma receita selecionada" })
+        //return;
+    //} else {
+        Recipe.find ({receitaSelecionada: true })
+              .then((recipes) => {
+                 response.status(200).json(recipes)
+              })
+              .catch(err => {
+                  response.status(400).json({ message: "erro"})
+              })
+    //}      
+}
+
+const getByNoChoseRecipe = (request, response) => {
+    Recipe.find ({receitaSelecionada: false })
+          .then((recipes) => {
+              response.status(200).json(recipes)
+          })
+          .catch(err => {
+              response.status(400).json({ message: "erro"})
+          })
 }
 
 
@@ -99,6 +124,25 @@ const updateRecipe = (request, response) => {
 
 }
 
+const uptdateChosenRecipe = (request, response) => {
+    const { id } = request.params
+    const { receitaSelecionada } = request.body
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({ message: "ID não é válido" })
+        return;
+    }else{
+    Recipe.findByIdAndUpdate ( id, {$set: {receitaSelecionada}})
+          .then ((recipe) => {
+              response.status(200).json ({ message: "Receita Selecionada"})
+          })
+          .catch ((err) => {
+              response.json(err)
+          })
+    }      
+
+}
+
 const updateIngredients = (req, res) => {
     const { id } = req.params
     const { ingredientes } = req.body
@@ -110,6 +154,101 @@ const updateIngredients = (req, res) => {
         Recipe.findByIdAndUpdate(id, { $set: { ingredientes } })
             .then(() => {
                 res.status(200).json({ message: `Os ingredientes da receita foram atualizados` })
+            })
+            .catch((err) => {
+                res.json(err)
+            })
+
+    }
+}
+
+const updateMainIngredient = (req, res) => {
+    const { id } = req.params
+    const { ingredientePrincipal } = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({ message: "ID não é válido" })
+        return;
+    } else {
+        Recipe.findByIdAndUpdate(id, { $set: { ingredientePrincipal } })
+            .then(() => {
+                res.status(200).json({ message: `Os ingredientes principal da receita foi atualizado` })
+            })
+            .catch((err) => {
+                res.json(err)
+            })
+
+    }
+}
+
+const updateTitle = (req, res) => {
+    const { id } = req.params
+    const { nomeReceita } = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({ message: "ID não é válido" })
+        return;
+    } else {
+        Recipe.findByIdAndUpdate(id, { $set: { nomeReceita } })
+            .then(() => {
+                res.status(200).json({ message: `O nome da receita foi atualizado` })
+            })
+            .catch((err) => {
+                res.json(err)
+            })
+
+    }
+}
+
+const updateType = (req, res) => {
+    const { id } = req.params
+    const { tipoReceita } = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({ message: "ID não é válido" })
+        return;
+    } else {
+        Recipe.findByIdAndUpdate(id, { $set: { tipoReceita } })
+            .then(() => {
+                res.status(200).json({ message: `O tipo de receita foi modificado` })
+            })
+            .catch((err) => {
+                res.json(err)
+            })
+
+    }
+}
+
+const updatePreparation = (req, res) => {
+    const { id } = req.params
+    const { preparo } = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({ message: "ID não é válido" })
+        return;
+    } else {
+        Recipe.findByIdAndUpdate(id, { $set: { preparo } })
+            .then(() => {
+                res.status(200).json({ message: `O modo de preparo foi modificadp` })
+            })
+            .catch((err) => {
+                res.json(err)
+            })
+
+    }
+}
+
+const updateComment = (req, res) => {
+    const { id } = req.params
+    const { observacoes } = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({ message: "ID não é válido" })
+        return;
+    } else {
+        Recipe.findByIdAndUpdate(id, { $set: { observacoes } })
+            .then(() => {
+                res.status(200).json({ message: `O campo observações foi modificado` })
             })
             .catch((err) => {
                 res.json(err)
@@ -136,11 +275,19 @@ module.exports = {
     getAll,
     getId,
     getByTitle,
-    getByingredient,
+    getByMainIngredient,
     getByTypeFood,
+    getByChoseRecipe,
+    getByNoChoseRecipe,
     createRecipe,
     updateRecipe,
+    uptdateChosenRecipe, 
     updateIngredients,
+    updateMainIngredient,
+    updateTitle,
+    updateType,
+    updatePreparation,
+    updateComment, 
     deleteRecipe
 }
 
--- mariana-franca-ta-na-mesa/src/routes/receitaRoute.js
+++ mariana-franca-ta-na-mesa/src/routes/receitaRoute.js
@@ -7,15 +7,31 @@ router.get("/", controller.getAll)
 
 router.get("/titulo", controller.getByTitle)
 
-router.get("/ingrediente", controller.getByingredient)
+router.get("/ingredienteprincipal", controller.getByMainIngredient)
 
 router.get("/tipo", controller.getByTypeFood)
+
+router.get ("/receitaselecionada", controller.getByChoseRecipe)
+
+router.get("/receitanaoselecionada", controller.getByNoChoseRecipe)
 
 router.post("/cadastro", controller.createRecipe)
 
 router.put("/editar/:id", controller.updateRecipe)
 
+router.patch("/receitanaoselecionada/:id", controller.uptdateChosenRecipe)
+
 router.patch("/editar/ingredientes/:id", controller.updateIngredients)
+
+router.patch("/editar/ingredienteprincipal/:id", controller.updateMainIngredient)
+
+router.patch("/editar/titulo/:id", controller.updateTitle)
+
+router.patch("/editar/tiporeceita/:id", controller.updateType)
+
+router.patch("/editar/preparo/:id", controller.updatePreparation)
+
+router.patch ("/editar/observacoes/:id", controller.updateComment)
 
 router.get("/:id", controller.getId)
 
