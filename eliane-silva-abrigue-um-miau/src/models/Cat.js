c3c3881e0ff6546b78a877e0d11df673b6e4f28e
Eliane Silva <eliane-asilva1@hotmail.com>
2020-12-08 19:17:06 -0300

    atualizando models e controllers

--- eliane-silva-abrigue-um-miau/src/models/Cat.js
+++ eliane-silva-abrigue-um-miau/src/models/Cat.js
@@ -4,8 +4,8 @@ const { Schema } = mongoose
 const catSchema = new Schema({
     responsible: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    contact: { type: Number, required: true },
     hashPass: { type: String, required: true },
+    contact: { type: Number, required: true },
     city: { type: String, required: true },
     neighborhood: { type: String, required: true },
     nicknameCat: { type: String, required: true },
