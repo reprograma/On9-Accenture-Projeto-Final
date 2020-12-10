137dafe09683f9c0d0a143e889ce1d0583652696
Rebeca Coelho <69407464+Rebeca-Coelho@users.noreply.github.com>
2020-12-10 14:19:52 -0300

    Update and rename laws.js to rightsValidator.js

--- rightsValidator.js
+++ rightsValidator.js
@@ -1,7 +1,8 @@
 const yup = require('yup')
 
-exports.lawSchema = yup.object().shape({
+exports.rightSchema = yup.object().shape({
     titleLegalSubject: yup.string().required('The title of legal subject is required'),
     sourceInformation: yup.string().required('The source information is required'),
     description: yup.string().required('The description is required'),
+    inclusionDate: yup.string().required('The date is required'),
 }).required('The form cannot be empty')
