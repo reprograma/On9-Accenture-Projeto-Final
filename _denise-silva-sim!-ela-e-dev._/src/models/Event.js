1e374ad430a592562a755f4ec100e83df6b2258a
Denise <deniseevelynk3@hotmail.com>
2020-12-10 17:42:15 -0300

    Models

--- _denise-silva-sim!-ela-e-dev._/src/models/Event.js
+++ _denise-silva-sim!-ela-e-dev._/src/models/Event.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
 
 const eventSchema = new mongoose.Schema({
   eventId: { type: Schema.Types.ObjectId, required: true },
