97389050b957b9b21c9d96a6da4ace52b1c8d2e2
Maria Eduarda Lima <meduardalima88@gmail.com>
2020-12-11 11:20:55 -0300

    Alteraçoes feitas mas o postman nao reconhece e fica dando invalido

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,35 +2,8 @@
 const mongoose = require('mongoose')
 const bcrypt = require("bcrypt");
 const bcryptSalt = 8;
-const Users = require('../models/User')
-
-exports.getAll = (req, res, next) => {
-    Users.find() 
-        .then((users) => {
-            res.status(200).json(users)
-        })
-        .catch(err => {
-        res.status(500).json({ message: err })
-        console.log('ERRO')
-        next(err)
-        })
-}
-
-exports.getByCity = (req, res, next) => {
-    const {city} = req.body;
-
-    Users.find({city: city})
-    .then((users) => {
-        res.status(200).json(users)
-    })
-    .catch(err => {
-        next(err)
-    })
-}
-
-/*exports.getByGameType = (req, res, next) {
-    const {type} = req.query
-} */
+const Users = require('../models/User');
+const User = require('../models/User');
 
 exports.post = async (req, res, next) => { 
     const { name, password, email, city, type, description } = req.body;
@@ -62,7 +35,39 @@ exports.post = async (req, res, next) => {
       res.status(400).json(err)
     })
     
-  }
+}
+
+exports.getAll = (req, res, next) => {
+  Users.find() 
+    .then((users) => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+      res.status(500).json({ message: err })
+      console.log('ERRO')
+      next(err)
+      })
+}
+
+
+exports.getByCity = (req, res) => {
+  const city = req.query.city
+
+    Users.find({ city: city, available: true }, { "name": 1, "email": 1, "password": 1, "city": 1, "type": 1, "description": 1})
+      .then((user) => {
+          response.status(200).json(user)
+
+
+      }).catch((error) => {
+            response.status(400).json({ error: 'City not found' })
+          })
+}
+
+/*exports.getByGameType = (req, res, next) {
+    const {type} = req.query
+} */
+
+
 
 
 //exports.put = (req, res, next)
