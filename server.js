0ed051bc4e52fcfd57314108c9cdd529915d2ec9
Manoela <manoela.pala@gmail.com>
2020-12-11 16:10:13 -0300

    atualizando cod para conexão com heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app')
 const port = 3000
 
-app.listen(process.env.PORT || port, (){
+app.listen(process.env.PORT || port, () => {
     console.log(`app rodando na porta ${port}`)
 })
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose');
+const dotenv =  require('dotenv');
+
+dotenv.config(); 
+
 mongoose.connect(`${process.env.dataBaseUrl}`, 
 {
     useNewUrlParser: true,
