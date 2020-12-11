382f0458735c19248bdfd3ebf1cefa7ee478bc3e
Michelle Silva <michellelicia98@gmail.com>
2020-12-11 12:44:18 -0300

    Adicionando Procfile

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-/* const app = require("./src/app")
-const port = 3001
-
-app.listen(port, function () {
-    console.log(`App is running at port: ${port}.`)
-})
- */
-
 const app = require('./src/app')
 const port = 5000
 
