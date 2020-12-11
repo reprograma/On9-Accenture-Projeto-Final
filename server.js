d0704f73d812682f7edd9a8addfc9405a9b159c1
Allana Caldas <allanacaldas@gmail.com>
2020-12-10 23:59:20 -0300

    Inserindo template string

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app.js");
 const port = 8080;
 
-app.listen(process.env.PORT || port, () => {
+app.listen(`${process.env.PORT}` || port, () => {
   console.log(`app está rodando na porta ${port}`);
 });
