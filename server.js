21c2f6558d1b5777a5454dd1dac00c656a96b8d9
Allana Caldas <allanacaldas@gmail.com>
2020-12-11 00:21:13 -0300

    Estabelecendo a versão do node no Package.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require("./src/app.js");
+const app = require("./src/app");
 const port = 8080;
 
-app.listen(`${process.env.PORT}` || port, () => {
-  console.log(`app está rodando na porta ${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(`app está rodando na porta on port  ${port}`, );
 });
