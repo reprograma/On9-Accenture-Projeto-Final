554ee21c123210110e3886ce4247b78e1fd9329c
aline-pereira <aline.g7.aa@gmail.com>
2020-12-11 20:42:28 -0300

    ajustando porta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
-const PORT = 8080
+const port = 8080
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}.`)
+app.listen(process.env.PORT || port, () => {
+    console.log(`Server is running at http://localhost:${port}.`)
     console.log('Press CTRL-C to stop\n')
 })
