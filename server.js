b171ae0f04ba419f377f8ef6bb76344e5088d4a3
Allana Caldas <allanacaldas@gmail.com>
2020-12-10 23:23:33 -0300

    Incluindo scripts no package.json

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-// const app = require('./src/app.js');
-// const PORT = 8080;
-
-// app.listen(PORT, () => { 
-//     console.log(`A aplicação está rodando na porta ${PORT}`)
-// })
-
 const app = require("./src/app.js");
 const port = 8080;
 
