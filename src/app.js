3fb4add0e90bb8d16e58b493ec10aba68882b9e2
Mariana França <donafran@gmail.com>
2020-12-11 14:26:58 -0300

    Heroku  e BD

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 const mongoose = require ("mongoose")
-mongoose.connect(`${process.env.DATABASE}`),
+mongoose.connect(`${process.env.DATABASE}`,
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
