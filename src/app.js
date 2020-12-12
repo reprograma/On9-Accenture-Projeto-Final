a45466209f5320059d7160318edd8d7d1e3269ad
thaispisani <thaispisani_@hotmail.com>
2020-12-12 06:25:12 -0300

    Projeto Final - Reprograma

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const mongoose = require('mongoose')
 mongoose.connect(`${process.env.DATABASE}`,
-    'mongodb://localhost/surfday',
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
