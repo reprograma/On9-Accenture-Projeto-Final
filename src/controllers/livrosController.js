0792be05a346b42b7b3a2f5506b9229075c25549
Íris Brito <irisbritox@gmail.com>
2020-12-11 22:44:22 -0300

    updating controller

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -54,7 +54,7 @@ const getDoesntHasTrigger = (request, response) =>{
 }
 
 const getByAuthor = (request, response) =>{
-    const { author } = request.query 
+    const { author } = request.params 
 
     Book.find(author)
         .then((books)=>{
@@ -64,7 +64,7 @@ const getByAuthor = (request, response) =>{
 }
 
 const getByTitle = (request, response) =>{
-    const { title } = request.body 
+    const { title } = request.params 
 
     Book.find(title)
         .then((books)=>{
