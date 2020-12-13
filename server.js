fba63958e9d95c731988c11f33bb1841470722af
gloriateodoro <gloriateodoro1@gmail.com>
2020-12-12 21:28:29 -0300

    Removendo comentários

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const app = require("./src/app");
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`app está rodando na porta ${port}`);
 });
 
-/*const app = require("./src/app")
-
-const PORT = 3000
-
-app.listen(PORT, function(){
-    console.log(`App de acessibilidade rodando na porta ${PORT}!!!`)
-})*/
 
