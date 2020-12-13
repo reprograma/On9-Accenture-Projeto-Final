d630ac34b8ccf27a7d17a07501e8fb12274b6185
gloriateodoro <gloriateodoro1@gmail.com>
2020-12-12 21:25:06 -0300

    userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,8 +6,14 @@ require('mongoose')
 //@route POST User
 //@descrição Registrar um novo usuário
 //@access Public
-//@endpoint http://localhost:porta/api/user/register
+//@endpoint https://viajante-sobre-rodas.herokuapp.com/api/user/register
 router.post("/register", userController.createUser)
+
+//@route GET User
+//@descrição Retornar todos os usuários cadastrados
+//@access Public (temporariamente)
+//@endpoint https://viajante-sobre-rodas.herokuapp.com/api/user/all
+router.get("/all", userController.allUsers)
 
 
 module.exports = router
