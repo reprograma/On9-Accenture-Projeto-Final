c129dbc71e1ce1f4492fc3328237bf2622742bd9
Luana Cristina de A Silva <luanacrystinne@msn.com>
2020-12-12 22:18:45 -0300

    excluindo o helpers

--- src/routes/transitAgent.js
+++ src/routes/transitAgent.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/transitAgent");
 const authMiddleware = require("../middlewares/auth");
 
-//@route GET
+//@route GET 
 router.get("/", controller.get)
 
 //@route POST 
