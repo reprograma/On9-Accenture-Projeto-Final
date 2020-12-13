28c9a521ea97594d7c7b21e98c7b524201af8223
gloriateodoro <gloriateodoro1@gmail.com>
2020-12-12 21:25:50 -0300

    Descrição de rota - autenticação

--- src/routes/authUserRoutes.js
+++ src/routes/authUserRoutes.js
@@ -3,6 +3,10 @@ const express = require("express")
 const router = express.Router()
 require('mongoose')
 
+//@route POST User
+//@descrição Autenticar login
+//@access Public (temporariamente)
+//@endpoint https://viajante-sobre-rodas.herokuapp.com/api/auth/authenticate
 router.post('/authenticate', authUserController.userAuthenticate)
 
 module.exports = router
